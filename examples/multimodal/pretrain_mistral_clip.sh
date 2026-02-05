f39873bd51d68752f3bcc779c753deb726e453e3
Faradawn Yang <73060648+faradawn@users.noreply.github.com>
2026-02-05 11:16:05 -0800

    fix triton cache patch

--- examples/multimodal/pretrain_mistral_clip.sh
+++ examples/multimodal/pretrain_mistral_clip.sh
@@ -21,14 +21,14 @@ TENSORBOARD_DIR="${OUTPUT}/tensorboard"
 
 export TRITON_CACHE_DIR="${WORKSPACE}/triton-cache/"
 # The following patch to the Triton cache manager is needed for Triton version <= 3.1
-export TRITON_CACHE_MANAGER="megatron.core.ssm.triton_cache_manager:ParallelFileCacheManager"
+# export TRITON_CACHE_MANAGER="megatron.core.ssm.triton_cache_manager:ParallelFileCacheManager"
 
 if [[ -z $LOAD_NAME ]]; then
     echo "Please set LOAD_NAME for input model name."
     exit 1
 fi
 
-CHECKPOINT_DIR="${WORKSPACE}/${LOAD_NAME}/checkpoints"
+CHECKPOINT_DIR="${WORKSPACE}/${LOAD_NAME}"
 
 DATA_TRAIN="${SOURCE}/examples/multimodal/pretrain_dataset.yaml"
 
